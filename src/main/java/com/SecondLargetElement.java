73dfe02b76f8959085f848a6896b5abdf1515d39
Namani Durga Srivas <148635901+Durgasrivas@users.noreply.github.com>
2026-02-06 11:12:44 +0530

    Add files via upload

---
+++ src/main/java/com/SecondLargetElement.java
@@ -0,0 +1,13 @@
+package com.codecounty.Second;
+
+import java.util.Scanner;
+
+public class SecondLargetElement {
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+    }
+}
